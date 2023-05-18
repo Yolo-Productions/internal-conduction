e4a4bd809846e222cc3726aad61678593729e5b2
skyprogrammer <arkhamphhyre2@gmail.com>
2023-05-18 07:53:18 -0300

    defined json module variable

--- yolocore/src/main/java/br/com/yolo/core/Client.java
+++ yolocore/src/main/java/br/com/yolo/core/Client.java
@@ -1,5 +1,6 @@
 package br.com.yolo.core;
 
+import br.com.yolo.core.storage.json.JsonModule;
 import br.com.yolo.core.storage.module.AccountModule;
 import lombok.Getter;
 
@@ -7,4 +8,6 @@ public class Client {
 
     @Getter
     private static final AccountModule accountModule = new AccountModule();
+    @Getter
+    private static final JsonModule jsonModule = new JsonModule();
 }
