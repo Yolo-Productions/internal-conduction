cabbb0dca2a9d41f54b15fd1c33b9896e5ad0d24
seaaland <gaabrielbruck@gmail.com>
2023-05-20 00:40:48 -0300

    constants

---
+++ yolocore/src/main/java/br/com/yolo/core/Constant.java
@@ -0,0 +1,12 @@
+package br.com.yolo.core;
+
+public class Constant {
+
+    public static final String
+            SERVER_NAME = "Yolo",
+            SERVER_COLORED_NAME = "§6§l" + SERVER_NAME.toUpperCase(),
+
+            ACCOUNT_NOT_LOADED = "§cHouve um erro ao carregar sua conta.",
+
+            REDIS_ACCOUNT_KEY = "account$";
+}
